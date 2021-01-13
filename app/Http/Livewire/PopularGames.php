6291f9962ad5142e6bc7afb79a0432061892acca
yxj0312 <yanxiajun@hotmail.com>
2021-01-13 23:15:21 +0100

    Use cache for speed

--- app/Http/Livewire/PopularGames.php
+++ app/Http/Livewire/PopularGames.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
@@ -13,22 +14,26 @@ class PopularGames extends Component
     public function loadPopularGames()
     {
         $before = Carbon::now()->subMonth(12)->timestamp;
-  
         $after = Carbon::now()->addMonth(12)->timestamp;
+
+        $this->popularGames = Cache::remember('popular-games', 7, function() use($before, $after){
+            sleep(3);
+            return Http::withHeaders(config('services.igdb'))
+            ->withBody(
+                "fields name, cover.url, first_release_date, platforms.abbreviation,rating, rating_count, slug;
+                    where platforms = (48,46,130,6)
+                    & rating != null
+                    & rating_count > 20
+                    & (first_release_date >= {$before}
+                    & first_release_date < {$after});
+                    sort first_release_date asc;
+                    sort rating desc;
+                    limit 12;",'text/plain')
+            ->post('https://api.igdb.com/v4/games/')
+            ->json();
+        });
         
-        $this->popularGames = Http::withHeaders(config('services.igdb'))
-        ->withBody(
-            "fields name, cover.url, first_release_date, platforms.abbreviation,rating, rating_count, slug;
-                where platforms = (48,46,130,6)
-                & rating != null
-                & rating_count > 20
-                & (first_release_date >= {$before}
-                & first_release_date < {$after});
-                sort first_release_date asc;
-                sort rating desc;
-                limit 12;",'text/plain')
-        ->post('https://api.igdb.com/v4/games/')
-        ->json();
+        
     }
 
     public function render()
