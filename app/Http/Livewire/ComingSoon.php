fcc2b57805bd27fbaf575b915d4332f6377c15c9
yxj0312 <yanxiajun@hotmail.com>
2021-01-19 22:02:23 +0100

    Refactor comingsoon

--- app/Http/Livewire/ComingSoon.php
+++ app/Http/Livewire/ComingSoon.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class ComingSoon extends Component
 {
@@ -14,20 +15,32 @@ class ComingSoon extends Component
     {
         $current = Carbon::now()->timestamp;
 
-        $this->comingSoon = Http::withHeaders(config('services.igdb'))
+        $comingSoonUnformatted = Http::withHeaders(config('services.igdb'))
         ->withBody(
-            "fields name, cover.url, first_release_date, platforms.abbreviation,summary;
+            "fields name, cover.url, first_release_date, platforms.abbreviation,summary,slug;
                 where platforms = (48,46,130,6)
                 & (first_release_date >= {$current});
                 sort first_release_date asc;
                 limit 4;",'text/plain')
         ->post('https://api.igdb.com/v4/games/')
         ->json();
+
+        $this->comingSoon = $this->formatForView($comingSoonUnformatted);
        
     }
 
     public function render()
     {
         return view('livewire.coming-soon');
+    }
+
+    private function formatForView($games)
+    {
+        return collect($games)->map(function ($game) {
+            return collect($game)->merge([
+                'coverImageUrl' => Str::replaceFirst('thumb','cover_small', $game['cover']['url']),
+                'releaseDate' => Carbon::parse($game['first_release_date'])->format('M d, Y'),
+            ]);
+        })->toArray();
     }
 }
