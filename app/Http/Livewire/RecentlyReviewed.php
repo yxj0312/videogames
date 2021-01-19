e4a20e3fd44c1c5772d02b1ff07e8b73129464d8
yxj0312 <yanxiajun@hotmail.com>
2021-01-19 22:19:54 +0100

    Refactor recently review

--- app/Http/Livewire/RecentlyReviewed.php
+++ app/Http/Livewire/RecentlyReviewed.php
@@ -5,9 +5,10 @@ namespace App\Http\Livewire;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class RecentlyReviewed extends Component
-{  
+{
     public $recentlyReviewed = [];
 
     public function loadRecentlyReviewed()
@@ -15,9 +16,9 @@ class RecentlyReviewed extends Component
         $before = Carbon::now()->subMonth(12)->timestamp;
         $current = Carbon::now()->timestamp;
 
-        $this->recentlyReviewed = Http::withHeaders(config('services.igdb'))
-        ->withBody(
-            "fields name, cover.url, first_release_date, platforms.abbreviation,rating, rating_count, summary;
+        $recentlyReviewedUnformatted = Http::withHeaders(config('services.igdb'))
+            ->withBody(
+                "fields name, cover.url, first_release_date, platforms.abbreviation,rating, rating_count, summary, slug;
                 where platforms = (48,46,130,6)
                 & rating != null
                 & rating_count > 20
@@ -25,13 +26,28 @@ class RecentlyReviewed extends Component
                 & first_release_date < {$current});
                 sort first_release_date asc;
                 sort rating desc;
-                limit 3;",'text/plain')
-        ->post('https://api.igdb.com/v4/games/')
-        ->json();
+                limit 3;",
+                'text/plain'
+            )
+            ->post('https://api.igdb.com/v4/games/')
+            ->json();
+
+        $this->recentlyReviewed = $this->formatForView($recentlyReviewedUnformatted);
     }
 
     public function render()
     {
         return view('livewire.recently-reviewed');
+    }
+
+    private function formatForView($games)
+    {
+        return collect($games)->map(function ($game) {
+            return collect($game)->merge([
+                'coverImageUrl' => Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']),
+                'rating' => isset($game['rating']) ? round($game['rating']) . '%' : null,
+                'platforms' => collect($game['platforms'])->pluck('abbreviation')->implode(', '),
+            ]);
+        })->toArray();
     }
 }
