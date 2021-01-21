f60c5e9155dec9059fa8b7091a8c1e0b4fc3005c
yxj0312 <yanxiajun@hotmail.com>
2021-01-21 22:16:30 +0100

    Add a partial blade for progressbar

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -23,38 +23,13 @@
                 <div class="flex flex-wrap items-center mt-8">
                     <div class="flex items-center">
                         <div id="memberRating" class="w-16 h-16 bg-gray-800 rounded-full relative text-sm">
-                        @push('scripts')
-                            <script>
-                                const progressBarContainer = document.getElementById('memberRating')
-
-                                var bar = new ProgressBar.Circle(progressBarContainer, {
-                                    color: 'white',
-                                    // This has to be the same size as the maximum width to
-                                    // prevent clipping
-                                    strokeWidth: 6,
-                                    trailWidth: 3,
-                                    trailColor: '#4A5568',
-                                    easing: 'easeInOut',
-                                    duration: 2500,
-                                    text: {
-                                        autoStyleContainer: false
-                                    },
-                                    from: { color: '#48BB78', width: 6 },
-                                    to: { color: '#48BB78', width: 6 },
-                                    // Set default step function for all animate calls
-                                    step: function(state, circle) {
-                                    circle.path.setAttribute('stroke', state.color);
-                                    circle.path.setAttribute('stroke-width', state.width);
-                                    var value = Math.round(circle.value() * 100);
-                                    if (value === 0) {
-                                        circle.setText('0%');
-                                    } else {
-                                        circle.setText(value+'%');
-                                    }
-                                    }
-                                });
-                            </script>
-                        @endpush
+                            @push('scripts')
+                                @include('_rating', [
+                                    'slug' => 'memberRating',
+                                    'rating' => $game['memberRating'],
+                                    'event' => null,
+                                ])
+                            @endpush
                             <!-- <div class="font-semibold text-xs flex justify-center items-center h-full">
                                 {{ $game['memberRating'] }}
                             </div> -->
