582e38ed42cb8433e435682e8c96e8fcc017a8f6
yxj0312 <yanxiajun@hotmail.com>
2021-01-22 23:27:46 +0100

    Add animation to popular games

--- resources/views/livewire/popular-games.blade.php
+++ resources/views/livewire/popular-games.blade.php
@@ -1,6 +1,6 @@
 <div wire:init="loadPopularGames" class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
     @forelse ($popularGames as $game)
-       <x-game-card :game="$game" /> 
+       <x-game-card :game="$game" />
     @empty
         @foreach (range(1, 12) as $game)
             <div class="game mt-8">
@@ -13,3 +13,9 @@
         @endforeach
     @endforelse
 </div> <!-- end popular-games -->
+
+@push('scripts')
+   @include('_rating', [
+        'event' => 'gameWithRatingAdded'
+    ])
+@endpush
