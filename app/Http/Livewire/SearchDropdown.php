b1237fddf826894c38f130ebae5952a8744e1db0
yxj0312 <yanxiajun@hotmail.com>
2021-01-23 23:03:55 +0100

    Backend check

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -13,12 +13,14 @@ class SearchDropdown extends Component
 
     public function render()
     {
-        $this->searchResults = Http::withHeaders(config('services.igdb.headers'))
-            ->withBody(
-                "search \"{$this->search}\";
-                fields name, cover.url, slug; limit 8;",'text/plain')
-            ->post(config('services.igdb.endpoint'))
-            ->json();
+        if (strlen($this->search) >= 2) {
+            $this->searchResults = Http::withHeaders(config('services.igdb.headers'))
+                ->withBody(
+                    "search \"{$this->search}\";
+                    fields name, cover.url, slug; limit 8;",'text/plain')
+                ->post(config('services.igdb.endpoint'))
+                ->json();
+        }
 
         return view('livewire.search-dropdown');
     }
