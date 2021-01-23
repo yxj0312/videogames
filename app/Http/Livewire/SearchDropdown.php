0b2d05a97544588395b8783ef8e5191282471611
yxj0312 <yanxiajun@hotmail.com>
2021-01-23 22:28:37 +0100

    Autocomplete implementation

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SearchDropdown extends Component
 {
+    public $search='';
+    public $searchResults = [];
+
+
     public function render()
     {
+        $this->searchResults = Http::withHeaders(config('services.igdb.headers'))
+            ->withBody(
+                "search \"{$this->search}\";
+                fields name, cover.url, slug; limit 8;",'text/plain')
+            ->post(config('services.igdb.endpoint'))
+            ->json();
+
         return view('livewire.search-dropdown');
     }
 }
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,17 +1,28 @@
 <div class="relative">
-    <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-3 pl-8 py-1 focus:outline-none focus:ring" placeholder="Search...">
+    <input 
+        wire:model.debounce.300ms="search"
+        type="text" 
+        class="bg-gray-800 text-sm rounded-full w-64 px-3 pl-8 py-1 focus:outline-none focus:ring" 
+        placeholder="Search..."
+    >
     <div class="absolute top-0 flex items-center h-full ml-2">
             <svg class="fill-current text-gray-400 w-4" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
     </div>
 
     <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2">
         <ul>
-            <li class="border-b border-gray-700">
-                <a href="#" class="block hover:bg-gray-700 flex items-center transition ease-in-out duration-150 px-3 py-3">
-                    <img src="https:://images.igdb.com/igdb/image/upload/t_cover_small/co1vcp.jpg" alt="cover" class="w-10">
-                    <span class="ml-4">Zelda</span>
-                </a>
-            </li>
+        	@foreach ($searchResults as $game )
+                <li class="border-b border-gray-700">
+                    <a href="{{ route('games.show', $game['slug'])}}" class="block hover:bg-gray-700 flex items-center transition ease-in-out duration-150 px-3 py-3">
+                        @isset($game['cover'])
+                        <img src="{{Str::replaceFirst('thumb', 'cover_small', $game['cover']['url'])}}" alt="cover" class="w-10">
+                        @else
+                        <img src="https://via.placeholder.com/264x352" alt="game cover" class="w-10">
+                        @endisset
+                        <span class="ml-4">{{ $game['name']}}</span>
+                    </a>
+                </li>
+            @endforeach
         </ul>
     </div>
 </div>
