c09440e0e2bbe706f93514e5f44115282cb2cf86
yxj0312 <yanxiajun@hotmail.com>
2021-01-24 23:12:53 +0100

    shift tab hidden list

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -7,6 +7,7 @@
         @focus="isVisible = true"
         @keydown.escape.window = "isVisible = false"
         @keydown="isVisible = true"
+        @keydown.shift.tab = "isVisible = false"
     >
     <div class="absolute top-0 flex items-center h-full ml-2">
         <svg class="fill-current text-gray-400 w-4" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
